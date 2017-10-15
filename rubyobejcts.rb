48d1a77d6adedb9c73458415d2d7378482da1147
Kevin <kevin.gallagher23@gmail.com>
2017-10-15 19:57:47 -0400

    made changes

--- rubyobejcts.rb
+++ rubyobejcts.rb
@@ -2,10 +2,7 @@
 # This is the calculator question
 
 attr_accessor :add, :subtract, :multiply, :divide, :value1, :value2
- # def initialize(value1,value2)
- #   @vaule1 = 0
- #   @value2 = 0
- # end
+
 
  def self.add(value1,value2)
    puts value1 + value2
